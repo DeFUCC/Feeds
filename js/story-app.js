bb9d8502cd96eb8605e42c5e9fc19b3a3e372419
DeFUCC <fond@fondfrukt.ru>
2014-04-01 21:17:25 +0400

    The story starts!

---
+++ js/story-app.js
@@ -0,0 +1,202 @@
+/**
+ * Created by starov on 01.04.14.
+ */
+var fruitStory = angular.module('fruitStory',[]);
+var controllers = {};
+fruitStory.controller(controllers);
+
+controllers.story = function ($scope) {
+    $scope.story = [
+        {
+            set:'B',
+            head:'Личность',
+            text:'— свод данных, характеризующих конкретное лицо или вымышленного персонажа',
+            next:[
+                {
+                    set:'A',
+                    head:'Личность, свод данных',
+                    text:'* любые авторские произведения,' +
+                        '* ник,' +
+                        '* ФИО,' +
+                        '* телефон' +
+                        '* email' +
+                        '* аккаунты в социальных сетях',
+                    next:[]
+                }
+            ]
+        },
+        {
+            set:'E',
+            head:'Лицо',
+            text:'— человек, подтвердивший свою личность',
+            next:[
+                {
+                    set:'Y',
+                    head:'',
+                    text:'Лицом может стать любое физическое лицо, способное подтвердить свою личность',
+                    next:[]
+                },
+                {
+                    set:'P',
+                    head:'Лицо затеи',
+                    text:'— конечный адресат всех вопросов, связанных с этой затеей. Глаза, уши, нос и рот затеи.',
+                    next:[]
+                }
+            ]
+        },
+        {
+            set:'A',
+            head:'Штука',
+            text:'— материальная сущность',
+            next:[]
+        },
+        {
+            set:'X',
+            head:'Задача',
+            text:'– описание результативного действия с приложением всей необходимой для его совершения информации',
+            next:[
+                {
+                    set:'A',
+                    head:'Полная задача',
+                    text:'подразумевает наличие инвентаря и необходимых материалов',
+                    next:[
+                        {
+                            set:'M',
+                            head:'Всегда есть несколько способов наполнения задачи для ее выполнения',
+                            text:'',
+                            next:[]
+                        }
+                    ]
+                },
+                {
+                    set:'O',
+                    head:'Выполнение задачи возмездно',
+                    text:'в зависимости от количества шагов, их сложности, а также от статуса собственности материалов и результатов выполнения задачи',
+                    next:[]
+                }
+            ]
+        },
+        {
+            set:'H',
+            head:'Затея',
+            text:'— план реализации общественно-значимых инфраструктурных объектов или событий',
+            next:[]
+        },
+        {
+            set:'C',
+            head:'Событие',
+            text:'— ограниченная по времени совместная деятельность или информация о ней, подтвержденная участниками',
+            next:[]
+        },
+        {
+            set:'P',
+            head:'Фонд развития',
+            text:'— открытый экономический интерфейс',
+            next:[]
+        },
+        {
+            set:'O',
+            head:'ФРУКТ',
+            text:'— это Фонд Развития Универсализации, Кооперации, Творчества',
+            next:[]
+        },
+        {
+            set:'M',
+            head:'Миссия ФРУКТа',
+            text:'Всестороннее разнонаправленное развитие творческого потенциала каждого человека в коллективной реализации затей',
+            next:[]
+        },
+        {
+            set:'Y',
+            head:'Универсализация',
+            text:'— реализация стремления каждого элемента системы овладеть практическим опытом многостороннего изучения всей системы',
+            next:[]
+        },
+        {
+            set:'K',
+            head:'Кооперация',
+            text:'— сложение всех возможных усилий для выполнения задач, невыполнимых индивидуально',
+            next:[]
+        },
+        {
+            set:'T',
+            head:'Творчество',
+            text:'— процесс осознанного приложения свободно определяемого усилия с целью увеличения меры совершенства окружающего мира',
+            next:[]
+        },
+        {
+            set:'HA',
+            head:'Навык',
+            text:'— умение реализовывать известные задачи или получать творческие результаты с применением соответствующего инструмента по назначению, а также руководство по овладению им',
+            next:[]
+        },
+        {
+            set:'BO',
+            head:'Лексическая форма',
+            text:'— форма применения слова в контексте языка',
+            next:[]
+        },
+        {
+            set:'BC',
+            head:'Высказывание',
+            text:'— любая информативная фраза, содержащая упоминание ее автора, а также хотя бы одного адресата или предмета этой фразы',
+            next:[]
+        },
+        {
+            set:'AP',
+            head:'Дар',
+            text:'— любая ценность, принятая фондом развития и направленная на реализацию его затей, а также информация о ней',
+            next:[]
+        }
+    ];
+
+    $scope.preset = preset;
+    $scope.colorize = colorize;
+
+    function colorize (set) {
+        var numOfSteps, step;
+        numOfSteps=Math.pow(12,set.length);
+        step=preset(set.length).indexOf(set.toUpperCase());
+        if (!~step) {step=Math.round(Math.random()*numOfSteps)} //if not set is not valid make random
+        return rainbow(numOfSteps,step+1);
+    };
+
+    function preset (bit){
+        var baseLetters = ['A', 'B', 'C', 'E', 'H', 'K', 'M', 'O', 'P', 'T', 'X', 'Y'];
+        var current, count, result = baseLetters.concat(), order;
+        if (typeof bit == "number" && bit>0 && bit<=12) {count=bit-1};
+        for (var b = 0; b < count; b++) {
+            current = result.slice(0);
+            order = 0;
+            for (var i = 0; i < current.length; i++) {
+
+                for (var j = 0; j < 12; j++) {
+
+                    result[order++] = current[i] + baseLetters[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    function rainbow (numOfSteps, step) {
+        // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distiguishable vibrant markers in Google Maps and other apps.
+        // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+        // Adam Cole, 2011-Sept-14
+        var r, g, b;
+        var h = step / numOfSteps;
+        var i = ~~(h * 6);
+        var f = h * 6 - i;
+        var q = 1 - f;
+        switch(i % 6){
+            case 0: r = 1, g = f, b = 0; break;
+            case 1: r = q, g = 1, b = 0; break;
+            case 2: r = 0, g = 1, b = f; break;
+            case 3: r = 0, g = q, b = 1; break;
+            case 4: r = f, g = 0, b = 1; break;
+            case 5: r = 1, g = 0, b = q; break;
+        }
+        var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+        return (c);
+    }
+};
